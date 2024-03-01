1fd186e1d15f33027fb387463fe58f3e81d6d289
jvinas <jvinasblasco@gmail.com>
2024-03-01 12:45:31 +0100

    (creo que así va el makefile)

--- claves.c
+++ claves.c
@@ -1,3 +1,7 @@
+#include <claves.h>
+#include <stdio.h>
+#include <mqueue.h>
+#include <stdlib.h>
 /*Desarrollar el código que implementa los servicios anteriores (init, set_value, get_value,
 delete_key, modify_value y exist). El código se desarrollará sobre el archivo con nombre
 claves.c. Este es el código que ofrece la interfaz a los clientes y se encarga de implementar
@@ -6,4 +10,12 @@ dicha implementación se deberá crear una biblioteca dinámica denominada libcl
 Esta será la biblioteca que utilizarán las aplicaciones de usuario que para usar el servicio. Debe
 investigar y buscar la forma de crear dicha biblioteca.*/
 // Created by jvinas on 2/26/24.
-//
+
+int init(){
+
+}
+/*int set_value(int key, char *value1, int N_value2, double *V_value2);
+int get_value(int key, char *value1, int *N_value2, double *V_value2);
+int delete_key(int key);
+int modify_value(int key, char *value1, int N_value2, double *V_value2);
+int exist(int key);*/
