3a0d87e21c2de5413f44846884e5547f94e37b07
jvinas <jvinasblasco@gmail.com>
2024-03-13 00:22:51 +0100

    basicamente de main he puesto lo mismo que habia en el otro archivo añadiendo el exist() que no estaba para probar todas las funciones

--- cliente.c
+++ cliente.c
@@ -14,7 +14,43 @@ haber ninguna referencia a colas de mensajes.*/
 #include <stdio.h>
 
 int main(){
-        int retorno = init();
-        printf("%dxdasdddf\n", retorno);
-        return 0;
+    init();
+    printf("------------\n");
+    char value1[] = "Sociedad";
+    double V_value2[] = {1.324, 22.2, 432.1};
+    set_value(13, value1, 3, V_value2);
+    printf("------------\n");
+    char value1_2 [] = "Pepperoni";
+    double V_value2_2[] = {32.2, 32.1, 3232.12, -12.0};
+    set_value(17, value1_2, 4, V_value2_2);
+    printf("------------\n");
+
+    char value1_3[256];
+    int N_value2_3;
+    double V_value2_3[32];
+    get_value(13, value1_3, &N_value2_3, V_value2_3);
+    printf("value1: %s\n", value1_3);
+    printf("N_value2: %d\n", N_value2_3);
+    for (int i = 0; i < N_value2_3; i++) {
+        printf("Elemento %d de V_value2: %f\n", i, V_value2_3[i]);
+    }
+
+
+    delete_key(17);
+
+
+    double V_value2_4[] = {3.21, 75.2, 342.2};
+    modify_value(13, "JUAN", 3, V_value2_4);
+    char value1_4[256];
+    int N_value2_4;
+    double V_value2_5[32];
+    get_value(13, value1_4, &N_value2_4, V_value2_5);
+    printf("value1: %s\n", value1_4);
+    printf("N_value2: %d\n", N_value2_4);
+    for (int i = 0; i < N_value2_4; i++) {
+        printf("Elemento %d de V_value2: %f\n", i, V_value2_5[i]);
+    }
+    printf("La tupla %d existe: %d\n", 17, exist(13));
+    exist(17);
+    return 0;
 };
