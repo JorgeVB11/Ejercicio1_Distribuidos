7d12713bcd0010b8ab666e8b90a422f609bf9395
jvinas <jvinasblasco@gmail.com>
2024-03-26 17:38:52 +0100

    prueba resubir anteriores ficheros

--- servidor.c
+++ servidor.c
@@ -3,7 +3,7 @@ que almacenan los elementos clave-valor1-valor2-valor3. Puede elegirse la estruc
 que se estime oportuno, siempre que no imponga un límite en el número de elementos que se
 pueden almacenar. El servidor desarrollado debe ser concurrente.*/
 // Created by jvinas on 2/26/24.
-//
+//Implementacion con sockets
 #include "proxy_server.h"
 // Mutex para el acceso a archivos
 pthread_mutex_t mutex_archivos;
